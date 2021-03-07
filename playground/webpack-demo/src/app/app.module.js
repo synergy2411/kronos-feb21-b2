c1db23bb1ccf33aa3c8606074b43c7bb6f3698ea
unknown <Sumit Khandelwal>
2021-03-07 16:32:18 +0530

    model separated

--- playground/webpack-demo/src/app/app.module.js
+++ playground/webpack-demo/src/app/app.module.js
@@ -1,21 +1,18 @@
+import { PRODUCTS } from './model/products';
 
 angular.module("app", ["emp-app"])
-.controller("MainController", function($scope){
-    $scope.product = product;
-    $scope.onBtnClick = function(){
-        alert("Who's this ?")
-    }
-})
+    .controller("MainController",["$scope", "$rootScope", function ($scope, $rootScope) {
+        // $scope.product = product;
+        $scope.products = PRODUCTS;
+        
+        $rootScope.favColor = "Green";
+        $scope.onBtnClick = function () {
+            alert("Who's this ?")
+        }
+    }])
 
-.controller("SecondController", function(){
-    this.message = "Controller Message";
-    
-})
+    .controller("SecondController", function ($rootScope) {
+        this.message = "Controller Message";
+        console.log("Fav Color : ", $rootScope.favColor);
+    })
 
-const product = {
-    name : "iPhone",
-    company : "Apple",
-    price : 70000,
-    description : "Awesome Camera",
-    isAvailabe : true
-}
---
+++ playground/webpack-demo/src/app/model/products.js
@@ -0,0 +1,23 @@
+export const PRODUCTS = [
+    {
+        name: "iPhone",
+        company: "Apple",
+        price: 70000,
+        description: "Awesome Camera",
+        isAvailabe: true
+    },
+    {
+        name: "S20",
+        company: "Samsung",
+        price: 90000,
+        description: "Great product",
+        isAvailabe: false
+    },
+    {
+        name: "Edge Plus",
+        company: "Motorola",
+        price: 60000,
+        description: "Awesome design",
+        isAvailabe: true
+    },
+]
